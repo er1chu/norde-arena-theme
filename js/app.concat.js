031824644239a8de54e9bf1cd80a02375612faec
Eric Hu <ericjhu@gmail.com>
2014-01-27 23:00:48 -0500

    removed unnecessary whitespace

--- js/app.concat.js
+++ js/app.concat.js
@@ -30017,12 +30017,12 @@ angular.module('ngResource', ['ng']).
 
 })(window, window.angular);
 
- var arena = angular.module('arenaApp', ['ngResource', 'ngSanitize']);
+var app = angular.module('arenaApp', ['ngResource', 'ngSanitize']);
 
-            arena.controller('channelShow', function($scope, $resource) { 
-              var Channel = $resource('http://api.are.na/v2/channels/:slug');
+app.controller('channelShow', function($scope, $resource) { 
+  var Channel = $resource('http://api.are.na/v2/channels/:slug');
 
-              var channel = Channel.get({slug: 'arena-influences', sort: 'position', direction: 'asc'}, function(){
-                $scope.channel = channel;
-              });
-            })
+  var channel = Channel.get({slug: 'arena-influences', sort: 'position', direction: 'asc'}, function(){
+    $scope.channel = channel;
+  });
+})
--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
- var arena = angular.module('arenaApp', ['ngResource', 'ngSanitize']);
+var app = angular.module('arenaApp', ['ngResource', 'ngSanitize']);
 
-            arena.controller('channelShow', function($scope, $resource) { 
-              var Channel = $resource('http://api.are.na/v2/channels/:slug');
+app.controller('channelShow', function($scope, $resource) { 
+  var Channel = $resource('http://api.are.na/v2/channels/:slug');
 
-              var channel = Channel.get({slug: 'arena-influences', sort: 'position', direction: 'asc'}, function(){
-                $scope.channel = channel;
-              });
-            })
+  var channel = Channel.get({slug: 'arena-influences', sort: 'position', direction: 'asc'}, function(){
+    $scope.channel = channel;
+  });
+})
